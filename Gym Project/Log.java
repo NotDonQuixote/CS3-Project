7898802eca0560cf5bb4e0f5ac43576e513ec109
NotDonQuixote <moralesf9825@gmail.com>
2025-11-14 20:14:40 -0700

    Added comments

--- Gym Project/Log.java
+++ Gym Project/Log.java
@@ -1,5 +1,8 @@
 public class Log {
     //Thanks to the Activity logger we may not actually neec this.
+
+    //Test for Github
+    //TODO: THis should appear
     String timestamp;
     String user;
     String action;
